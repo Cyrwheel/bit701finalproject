c2ef7856cf396bc250c11e86cb45fd451846344b
Cyrwheel <124426626+Cyrwheel@users.noreply.github.com>
2023-06-21 16:32:17 +0900

    first commit

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import bgimag from './s85.jpeg';
+function App() {
+  return (
+    <div className="App">
+      <img alt={''} src={bgimag} width={1000} border={"1"}/>
+      <br/><br/>
+      <h2>도커 성공기원 </h2>
+    </div>
+  );
+}
+
+export default App;
