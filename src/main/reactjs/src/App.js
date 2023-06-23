7cee4677caec9214b064b86cb250d8e24490a6d0
Cyrwheel <124426626+Cyrwheel@users.noreply.github.com>
2023-06-23 15:25:46 +0900

    저기여

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <img alt={''} src={bgimag} width={1000} border={"1"}/>
       <br/><br/>
-      <h2>도커 성공기원tq </h2>
+      <h2>도커 성공기원ㅇㄴㅇ </h2>
     </div>
   );
 }
